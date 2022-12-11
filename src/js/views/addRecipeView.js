a6bda32c142356801dcc92e67e38348e6feffccf
faried-elrewany <fariedezz4@gmail.com>
2022-12-11 02:02:35 +0200

    clean

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -3,12 +3,11 @@ import icons from 'url:../../img/icons.svg'; // Parcel 2
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
-  _message = 'Recipe was successfully uploaded :)';
-
   _window = document.querySelector('.add-recipe-window');
   _overlay = document.querySelector('.overlay');
   _btnOpen = document.querySelector('.nav__btn--add-recipe');
   _btnClose = document.querySelector('.btn--close-modal');
+  _message = 'Recipe was successfully uploaded :)';
 
   constructor() {
     super();
