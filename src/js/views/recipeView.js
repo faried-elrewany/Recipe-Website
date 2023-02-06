88651f1524c8536fc7a850b4a9006a44f0ac8d09
faried-elrewany <fariedezz4@gmail.com>
2023-02-06 04:29:59 +0200

    updat fractional and resposive

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,8 +1,7 @@
 import View from './View.js';
-
+import { numberToFraction } from '../helpers';
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');
@@ -128,7 +127,7 @@ class RecipeView extends View {
         <use href="${icons}#icon-check"></use>
       </svg>
       <div class="recipe__quantity">${
-        ing.quantity ? new Fraction(ing.quantity).toString() : ''
+        ing.quantity ? numberToFraction(ing.quantity).toString() : ''
       }</div>
       <div class="recipe__description">
         <span class="recipe__unit">${ing.unit}</span>
